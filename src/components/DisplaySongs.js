eab4b0ff62afdcf0966ae393e188924f0cb37775
Mathias2860DK <mathias.enemark.poulsen@gmail.com>
2021-11-23 21:33:21 +0100

    useEffect drillede. Men nu virker det. User Story #1 DONE

--- src/components/DisplaySongs.js
+++ src/components/DisplaySongs.js
@@ -1,3 +1,7 @@
+import Button from "@restart/ui/esm/Button";
+import { Card, CardGroup } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+
 function DisplaySongs({ tracks }) {
   //   console.log("song name: " + tracks.data.tracks.items[0].name);
   //   console.log("Artist name: " + tracks.data.tracks.items[0].artists[0].name);
@@ -17,24 +21,44 @@ function DisplaySongs({ tracks }) {
       {
         <ul>
           {tracks.data.tracks.items.map((track, index) => (
-            <tr>
-              <td>{track.name}</td>
-              <td>{track.artists[0].name}</td>
-              <td>
-                <a href={track.external_urls.spotify}>
-                  <img
-                    className="album-cover"
-                    alt="album cover"
-                    src={track.album.images[0].url}
-                  />
-                </a>
-              </td>
-            </tr>
+            <>
+             
+                  <CardGroup>
+                    <Card>
+                      <Card.Body>
+                        <Card.Title>{track.name}</Card.Title>
+                        <Card.Text>{track.artists[0].name}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                    <Card>
+                      <a href={track.external_urls.spotify}>
+                        <Card.Img
+                          variant="center"
+                          className="album-cover img-fluid "
+                          src={track.album.images[0].url}
+                        />
+                      </a>
+                    </Card>
+                  </CardGroup>
+               
+            </>
+            // <tr>
+            //   <td>{track.name}</td>
+            //   <td>{track.artists[0].name}</td>
+            //   <td>
+            //     <a href={track.external_urls.spotify}>
+            //       <img
+            //         className="album-cover"
+            //         alt="album cover"
+            //         src={track.album.images[0].url}
+            //       />
+            //     </a>
+            //   </td>
+            // </tr>
           ))}
         </ul>
       }
     </div>
-    
   );
 }
 
--- src/components/RandomSong.js
+++ src/components/RandomSong.js
@@ -1,14 +1,46 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DisplaySongs from "./DisplaySongs";
-function RandomSong({ token }) {
+function RandomSong() {
   const [tracks, setTracks] = useState("");
+  const [token, setToken] = useState("");
+
 
    useEffect(() => {
-     getRandomSong()
-    
-   }, []);
+       //api keys
+     var clientId = process.env.REACT_APP_CLIENT_ID;
+     var clientSecret = process.env.REACT_APP_CLIENT_SECRET;
 
+     //Get the token from the keys
+     axios("https://accounts.spotify.com/api/token", {
+       headers: {
+         "Content-Type": "application/x-www-form-urlencoded",
+         Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+       },
+       data: "grant_type=client_credentials",
+       method: "POST",
+     }).then((tokenResponse) => {
+       setToken(tokenResponse.data.access_token);
+       console.log(tokenResponse.data.access_token);
+        const randomWildcard = getRandomSearch();
+        console.log(randomWildcard);
+        //fetch 20 random tracks
+        axios(
+          `https://api.spotify.com/v1/search?q=${randomWildcard}25&type=track`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: "Bearer " + tokenResponse.data.access_token,
+            },
+          }
+        ).then((tracksResponse) => {
+          setTracks(tracksResponse);
+          console.log(tracksResponse);
+        });
+     });
+
+   }, []);
+   
   function getRandomSearch() {
     // A list of all characters that can be chosen.
     const characters = "abcdefghijklmnopqrstuvwxyz";
@@ -18,33 +50,12 @@ function RandomSong({ token }) {
       Math.floor(Math.random() * characters.length)
     );
     let randomSearch = "";
-        randomSearch = randomCharacter + "%";
+    randomSearch = randomCharacter + "%";
     return randomSearch;
   }
-
-  const getRandomSong = (evt) => {
-    console.log(token);
-    const randomWildcard = getRandomSearch();
-    console.log(randomWildcard);
-    axios(
-      `https://api.spotify.com/v1/search?q=${randomWildcard}25&type=track`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      }
-    ).then((tracksResponse) => {
-      setTracks(tracksResponse)
-      console.log(tracksResponse);
-
-    });
-  };
-
   return (
     <div>
-      
-      {tracks!=="" ?<DisplaySongs tracks={tracks}/> : ""}
+      {tracks !== "" ? <DisplaySongs tracks={tracks} /> : ""}
     </div>
   );
 }
